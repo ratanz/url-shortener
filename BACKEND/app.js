adda4825caa2d6e3b5332c9c8cb72fb64fa45d36
Ratan <ratanrathod870@gmail.com>
2025-05-19 18:21:22 +0530

    added express

---
+++ BACKEND/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {nanoid} from 'nanoid';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/api/create', (req, res) => {
+    const {url} = req.body;
+   res.send(nanoid(7)) 
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000');
+});
+
