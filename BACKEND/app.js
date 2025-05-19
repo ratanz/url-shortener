0e4e09ba38726323aa60aa925a47c7f881fea3ff
Ratan <ratanrathod870@gmail.com>
2025-05-19 20:39:23 +0530

    added basic backend setup - connected db

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,17 +1,30 @@
 import express from 'express';
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+dotenv.config('./.env');
+import connectDB from './src/config/mongoconfig.js';
+import urlSchema from './src/models/shorturl.model.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/create', (req, res) => {
-    const {url} = req.body;
-   res.send(nanoid(7)) 
+app.post('/api/create', (req, res) => {
+    const { url } = req.body;
+    const shortUrl = nanoid(7);
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl,
+    });
+    newUrl.save();
+    res.send(nanoid(7))
 });
 
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running on port http://localhost:3000');
 });
 
+
+// GET - Redirection
