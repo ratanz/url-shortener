c96d4037dd452d234ff57929b23bdc8c23e57a91
Ratan <ratanrathod870@gmail.com>
2025-05-20 18:37:55 +0530

    added url duplicate prevention

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -10,21 +10,54 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/create', (req, res) => {
+// app.post('/api/create', (req, res) => {
+//     const { url } = req.body;
+//     const shortUrl = nanoid(7);
+//     const newUrl = new urlSchema({
+//         full_url: url,
+//         short_url: shortUrl,
+//     });
+//     newUrl.save();
+//     res.send(nanoid(7))
+// });
+
+// prevents duplicate URL entries
+app.post('/api/create', async (req, res) => {
     const { url } = req.body;
-    const shortUrl = nanoid(7);
-    const newUrl = new urlSchema({
-        full_url: url,
-        short_url: shortUrl,
-    });
-    newUrl.save();
-    res.send(nanoid(7))
+    try {
+        // Check if the URL already exists
+        let existing = await urlSchema.findOne({ full_url: url });
+        if (existing) {
+            // Return the existing short URL
+            return res.json({ shortUrl: existing.short_url, message: 'URL already exists' });
+
+        }
+        // Create a new short URL
+        const shortUrl = nanoid(7);
+        const newUrl = new urlSchema({
+            full_url: url,
+            short_url: shortUrl,
+        });
+        await newUrl.save();
+        res.json({  nanoid: shortUrl, message: 'URL created successfully' });
+    } catch (err) {
+        res.status(500).json({ error: 'Server error' });
+    }
 });
+
+app.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const url = await urlSchema.findOne({ short_url: id});
+    if (url) {
+        res.redirect(url.full_url);
+    }
+    else {
+        res.status(404).send('URL not found');
+    }
+});
+
 
 app.listen(3000, () => {
     connectDB();
     console.log('Server is running on port http://localhost:3000');
 });
-
-
-// GET - Redirection
